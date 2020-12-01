bfb931b35eb9e15da69262193b98222547ac04c6
Victor Roest <victor@xirion.net>
2020-12-01 16:56:32 +0100

    day1: small cleanup

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,24 +1,21 @@
 defmodule DayOne do
   def parse(file) do
-    f =
-      File.read!(file)
-      |> String.trim()
-      |> String.split("\n")
-
-    Enum.map(f, &String.to_integer/1)
+    File.read!(file)
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
 
   def part1 do
     input = parse("./inputs/day1.txt")
     adds_to_2020 = fn a, b -> a + b == 2020 end
-    res = for x <- input, y <- input, adds_to_2020.(x, y), do: x * y
-    hd(res)
+    [h | _] = for x <- input, y <- input, adds_to_2020.(x, y), do: x * y
+    h
   end
 
   def part2 do
     input = parse("./inputs/day1.txt")
     adds_to_2020 = fn a, b, c -> a + b + c == 2020 end
-    res = for x <- input, y <- input, z <- input, adds_to_2020.(x, y, z), do: x * y * z
-    hd(res)
+    [h | _] = for x <- input, y <- input, z <- input, adds_to_2020.(x, y, z), do: x * y * z
+    h
   end
 end
