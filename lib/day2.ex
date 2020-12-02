bbe1afe2eeb8ffd986cbc2bfee34e4757a73de6c
Victor Roest <victor@xirion.net>
2020-12-02 09:29:09 +0100

    day2 done!

---
+++ lib/day2.ex
@@ -0,0 +1,32 @@
+defmodule DayTwo do
+  def parse(file) do
+    File.read!(file)
+    |> String.split("\n", trim: true)
+  end
+
+  def is_valid(line) do
+    [min, max, char, password] = String.split(line, ["-", " ", ":"], trim: true)
+    [c | _] = to_charlist(char)
+    count = Enum.count(to_charlist(password), &(&1 == c))
+
+    count >= String.to_integer(min) && count <= String.to_integer(max)
+  end
+
+  def is_valid2(line) do
+    [pos1, pos2, char, password] = String.split(line, ["-", " ", ":"], trim: true)
+    at_pos1 = String.at(password, String.to_integer(pos1) - 1) == char
+    at_pos2 = String.at(password, String.to_integer(pos2) - 1) == char
+
+    (at_pos1 && !at_pos2) || (!at_pos1 && at_pos2)
+  end
+
+  def part1 do
+    parse("./inputs/day2.txt")
+    |> Enum.count(&is_valid/1)
+  end
+
+  def part2 do
+    parse("./inputs/day2.txt")
+    |> Enum.count(&is_valid2/1)
+  end
+end
---
+++ test/day2_test.exs
@@ -0,0 +1,25 @@
+defmodule DayTwoTest do
+  use ExUnit.Case
+
+  import DayTwo
+
+  test "is_valid" do
+    assert is_valid("1-3 a: abcde")
+    assert !is_valid("1-3 b: cdefg")
+    assert is_valid("2-9 c: ccccccccc")
+  end
+
+  test "is_valid2" do
+    assert is_valid2("1-3 a: abcde")
+    assert !is_valid2("1-3 b: cdefg")
+    assert !is_valid2("2-9 c: ccccccccc")
+  end
+
+  test "part1" do
+    assert part1() == 424
+  end
+
+  test "part2" do
+    assert part2() == 747
+  end
+end
