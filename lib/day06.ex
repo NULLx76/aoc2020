b2a24bc14c7f3105e265e9f5ce49cf4b70757df2
Victor Roest <victor@xirion.net>
2020-12-06 09:53:27 +0100

    Day 6

--- lib/day06.ex
+++ lib/day06.ex
@@ -1,10 +1,32 @@
 defmodule Day06 do
   @moduledoc "Day Six of the AoC"
 
-  def parse(file \\ "./inputs/day6.txt") do
-    File.read!(file)
-    |> String.split("\n", trim: true)
+  defp count_answers(list, :anyone), do: count_answers(list, &MapSet.union/2)
+  defp count_answers(list, :everyone), do: count_answers(list, &MapSet.intersection/2)
+
+  defp count_answers(list, f) do
+    list
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.map(&MapSet.new/1)
+    |> Enum.reduce(f)
+    |> Enum.count()
   end
 
-  def part1, do: nil
+  defp parse(file) do
+    File.read!(file)
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&String.split/1)
+  end
+
+  def part1(file \\ "./inputs/day6.txt") do
+    parse(file)
+    |> Enum.map(&count_answers(&1, :anyone))
+    |> Enum.sum()
+  end
+
+  def part2(file \\ "./inputs/day6.txt") do
+    parse(file)
+    |> Enum.map(&count_answers(&1, :everyone))
+    |> Enum.sum()
+  end
 end
--- test/day06_test.exs
+++ test/day06_test.exs
@@ -1,4 +1,12 @@
 defmodule Day06Test do
   use ExUnit.Case, async: true
-  # import Day06
+  import Day06
+
+  test "part1" do
+    assert part1() == 6930
+  end
+
+  test "part2" do
+    assert part2() == 3585
+  end
 end
