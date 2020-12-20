1d314d982a69c101b65c0ee499e8826dee4e875d
Victor Roest <victor@xirion.net>
2020-12-20 18:06:37 +0100

    Day 20, part 1

---
+++ lib/day20.ex
@@ -0,0 +1,61 @@
+defmodule Day20 do
+  def parse(input) do
+    input
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn tile ->
+      [header | [data]] = tile |> String.split(":")
+      "Tile " <> num = header
+      {String.to_integer(num), String.split(data, "\n", trim: true)}
+    end)
+  end
+
+  def get_edges(lines) do
+    top = Enum.at(lines, 0)
+    bottom = Enum.at(lines, -1)
+
+    left =
+      lines
+      |> Enum.map(&String.first/1)
+      |> Enum.join()
+
+    right =
+      lines
+      |> Enum.map(&String.last/1)
+      |> Enum.join()
+
+    [
+      top,
+      top |> String.reverse(),
+      bottom,
+      bottom |> String.reverse(),
+      left,
+      left |> String.reverse(),
+      right,
+      right |> String.reverse()
+    ]
+  end
+
+  def share_edge?({a, _}, {b, _}) when a == b, do: false
+  def share_edge?({_, a}, {_, b}), do: Enum.any?(a, &(&1 in b))
+
+  def find_corners(tiles), do: find_corners(tiles, tiles, 1)
+  def find_corners(_, [], acc), do: acc
+
+  def find_corners(all, [head | tail], acc) do
+    case Enum.count(all, &share_edge?(&1, head)) do
+      2 -> find_corners(all, tail, elem(head, 0) * acc)
+      _ -> find_corners(all, tail, acc)
+    end
+  end
+
+  def part1(file \\ "./inputs/day20.txt") do
+    File.read!(file)
+    |> parse()
+    |> Enum.map(fn {num, lines} -> {num, get_edges(lines)} end)
+    |> find_corners()
+  end
+
+  def part2 do
+    :todo
+  end
+end
---
+++ test/day20_test.exs
@@ -0,0 +1,13 @@
+defmodule Day20Test do
+  use ExUnit.Case, async: true
+
+  import Day20
+
+  test "part1" do
+    assert part1() == 54_755_174_472_007
+  end
+
+  # test "part2" do
+  #   assert part2() == 363
+  # end
+end
